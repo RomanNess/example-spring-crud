9d7556c99ff554907c86da353934391976e92272
Roman Ness <roman.ness@cosee.biz>
2020-10-30 10:48:52 +0100

    port demo in Application class over to unit tests

--- src/test/java/one/microstream/examples/springcrud/CustomerRepositoryTest.java
+++ src/test/java/one/microstream/examples/springcrud/CustomerRepositoryTest.java
@@ -1,5 +1,6 @@
 package one.microstream.examples.springcrud;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,13 +13,55 @@ class CustomerRepositoryTest {
     @Autowired
     private CustomerRepository customerRepository;
 
+    @BeforeEach
+    void initData() {
+        customerRepository.deleteAll();
+
+        long customerNr = 1L;
+        customerRepository.add(new Customer(customerNr++, "Thomas", "Wresler"));
+        customerRepository.add(new Customer(customerNr++, "Jim"   , "Joe"));
+        customerRepository.add(new Customer(customerNr++, "Kamil" , "Limitsky"));
+        customerRepository.add(new Customer(customerNr++, "Karel" , "Ludvig"));
+    }
+
+    @Test
+    void findAll() {
+        assertThat(customerRepository.findAll())
+                .hasSize(4)
+                .extracting(Customer::getFirstName).containsExactlyInAnyOrder("Thomas", "Jim", "Kamil", "Karel");
+    }
+
+    @Test
+    void findByFirstName() {
+        assertThat(customerRepository.findByFirstName("Jim"))
+                .containsExactly(Customer.builder().customerNumber(2L).firstName("Jim").lastName("Joe").build());
+    }
+
+    @Test
+    void updateAll() {
+        customerRepository.findAll().forEach(c -> c.setFirstName("Johan"));
+        customerRepository.storeAll();
+
+        assertThat(customerRepository.findAll())
+                .hasSize(4)
+                .extracting(Customer::getFirstName).containsOnly("Johan");
+    }
+
+    @Test
+    void deleteAll() {
+        customerRepository.deleteAll();
+        assertThat(customerRepository.findAll()).isEmpty();
+    }
+
     @Test
     void save() {
-        assertThat(customerRepository.findAll()).isEmpty();
+        assertThat(customerRepository.findAll()).hasSize(4);
 
         Customer customer = Customer.builder().customerNumber(1L).firstName("Hans").lastName("Wurst").build();
         customerRepository.add(customer);
 
-        assertThat(customerRepository.findAll()).containsExactly(customer);
+        assertThat(customerRepository.findAll())
+                .hasSize(5)
+                .contains(customer);
     }
 }
